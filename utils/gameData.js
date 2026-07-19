2ea8d9472a8bac9658bfb6407a548d2acc9cd2a4
LI-ZETH <genesis.luo@utp.ac.pa>
2026-07-19 14:18:19 -0500

    Update gameData.js
    game data

--- utils/gameData.js
+++ utils/gameData.js
@@ -1,8 +1,8 @@
 export const figures = [
-  { id: 'triangulo', name: 'Triángulo', memoryImage: require('../assets/img/CARTA_TRIANGULO.png'), image: require('../assets/img/TRIANGULO.png'), audio: require('../assets/audio/perro.wav') },
-  { id: 'cuadrado', name: 'Cuadrado', memoryImage: require('../assets/img/CARTA_CUADRADO.png'), image: require('../assets/img/CUADRADO.png'), audio: require('../assets/audio/gato.wav') },
-  { id: 'circulo', name: 'Círculo', memoryImage: require('../assets/img/CARTA_CIRCULO.png'), image: require('../assets/img/CIRCULO.png'), audio: require('../assets/audio/carro.wav') },
-  { id: 'estrella', name: 'Estrella', memoryImage: require('../assets/img/CARTA_ESTRELLA.png'), image: require('../assets/img/ESTRELLA.png'), audio: require('../assets/audio/manzana.wav') },
+  { id: 'triangulo', name: 'Triángulo', memoryImage: require('../assets/img/CARTA_TRIANGULO.png'), image: require('../assets/img/TRIANGULO.png'), audio: require('../assets/audio/Triangulo.mp3') },
+  { id: 'cuadrado', name: 'Cuadrado', memoryImage: require('../assets/img/CARTA_CUADRADO.png'), image: require('../assets/img/CUADRADO.png'), audio: require('../assets/audio/Cuadrado.mp3') },
+  { id: 'circulo', name: 'Círculo', memoryImage: require('../assets/img/CARTA_CIRCULO.png'), image: require('../assets/img/CIRCULO.png'), audio: require('../assets/audio/Circulo.mp3') },
+  { id: 'estrella', name: 'Estrella', memoryImage: require('../assets/img/CARTA_ESTRELLA.png'), image: require('../assets/img/ESTRELLA.png'), audio: require('../assets/audio/Estrella.mp3') },
 ];
 
 export const memoryPairs = figures.slice(0, 3);
